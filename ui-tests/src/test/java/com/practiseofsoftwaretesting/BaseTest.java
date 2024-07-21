ad09844169dcfa710435356ec813f69f55cd1be1
nikitinakateryna <kateryna.nikitina@appflame.com>
2024-07-21 13:00:50 +0300

    Move Selenide configuration

--- ui-tests/src/test/java/com/practiseofsoftwaretesting/BaseTest.java
+++ ui-tests/src/test/java/com/practiseofsoftwaretesting/BaseTest.java
@@ -4,11 +4,20 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterEach;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
+import com.practicesoftwaretesting.utils.ConfigReader;
 
 public abstract class BaseTest {
 
+    static ConfigReader configReader = new ConfigReader();
+
     static {
-        Configuration.baseUrl = "https://practicesoftwaretesting.com/";
+        Configuration.baseUrl = configReader.getProperty("base.url");
+        Configuration.timeout = Long.parseLong(configReader.getProperty("timeout"));
+        Configuration.browserSize = configReader.getProperty("browser.size");
+        Configuration.clickViaJs = Boolean.parseBoolean(configReader.getProperty("click.via.js"));
+        Configuration.fastSetValue = Boolean.parseBoolean(configReader.getProperty("fast.set.value"));
+        Configuration.headless = Boolean.parseBoolean(configReader.getProperty("headless"));
+        Configuration.proxyEnabled = Boolean.parseBoolean(configReader.getProperty("proxy.enabled"));
     }
 
     @AfterEach
